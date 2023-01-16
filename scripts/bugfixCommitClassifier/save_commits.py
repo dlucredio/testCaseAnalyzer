b4579a218aea215359cb2758b0d5ac30dca249e6
Daniel Lucrédio <daniel.lucredio@ufscar.br>
2023-01-16 15:31:45 -0300

    Training commits

--- scripts/bugfixCommitClassifier/save_commits.py
+++ scripts/bugfixCommitClassifier/save_commits.py
@@ -1,8 +1,11 @@
 # This scripts loads commits from smartshark 2.2 and saves them as commits.jsonl file
 # Only _id, revision_hash and message are extracted
+# Uses packages from requirements.txt
+# Tested on Python 3.11.1
 
 from pymongo import MongoClient
-import json
+import pandas as pd
+import psutil
 
 client = MongoClient("mongodb://localhost:27017/")
 
@@ -12,13 +15,25 @@ commit_collection = smartshark['commit']
 query = {}
 projection = {'revision_hash': 1, 'message': 1}
 
-commits = commit_collection.find(query, projection, batch_size=1000)
+commits = commit_collection.find(query, projection, batch_size=500)
 
 i=0
-with open("commits.jsonl",'w') as f:
-    for c in commits:
-        f.write(json.dumps({'_id': str(c['_id']),'revision_hash':c['revision_hash'], 'message':c['message']}) + "\n")
-        print(f'Processing commit number {i}')
-        i += 1
+
+commits_list = []
+
+for c in commits:
+    commits_list.append({
+        '_id': str(c['_id']),
+        'revision_hash':c['revision_hash'],
+        'message':c['message']
+    })
+    if i % 100 == 0:
+        print(f'Processing commit number {i} - RAM Used (GB): {psutil.virtual_memory()[3]/1000000}')
+    i += 1
 
 print(f'Done! {i} commits processed')
+
+dfCommits = pd.DataFrame(commits_list, columns=['_id','revision_hash','message'])
+dfCommits.to_parquet('commits.parquet.gzip',compression='gzip')
+
+print('Saved data to file "commits.parquet.gzip"')
